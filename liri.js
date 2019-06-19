b27e02dbcd7b2ef08257632e889b89f431390e30
Saron <saronnhong@gmail.com>
2019-06-19 16:53:59 -0700

    fix bugs and add screenshots and video

--- liri.js
+++ liri.js
@@ -13,22 +13,12 @@ function callSpotify(spotCallQuery) {
     spotify
         .search({ type: 'track', query: spotCallQuery })
         .then(function (response) {
-            console.log(response.tracks.items.length);
-            // checks if there was a result to the search
-            if (response.tracks.items.length) {
+            for (var i = 0; i < response.tracks.items.length; i++) {
+                console.log("Artist: " + response.tracks.items[i].album.artists[0].name + "\n" + "Song name: " + response.tracks.items[i].name + "\n" + "Preview URL: " + response.tracks.items[i].preview_url + "\n" + "Album name: " + response.tracks.items[i].album.name + "\n");
 
-                for (var i = 0; i < response.tracks.items.length; i++) {
-                    console.log("Artist: " + response.tracks.items[i].album.artists[0].name + "\n" + "Song name: " + response.tracks.items[i].name + "\n" + "Preview URL: " + response.tracks.items[i].preview_url + "\n" + "Album name: " + response.tracks.items[i].album.name + "\n");
+                appendFunc("Artist: " + response.tracks.items[i].album.artists[0].name + "\n" + "Song name: " + response.tracks.items[i].name + "\n" + "Preview URL: " + response.tracks.items[i].preview_url + "\n" + "Album name: " + response.tracks.items[i].album.name + "\n");
+            }
 
-                    appendFunc("Artist: " + response.tracks.items[i].album.artists[0].name + "\n" + "Song name: " + response.tracks.items[i].name + "\n" + "Preview URL: " + response.tracks.items[i].preview_url + "\n" + "Album name: " + response.tracks.items[i].album.name + "\n");   
-                }
-            }
-            else {
-                //if no result was found in the search
-                //console.log("no results returned");
-                var spotQuery = "the sign ace of base"
-                callSpotify(spotQuery);
-            }
         })
         .catch(function (err) {
             console.log(err);
@@ -40,17 +30,16 @@ function callImdbSearch(movieNameSearch) {
     axios
         .get(imdbQueryUrl)
         .then(function (response) {
-            if (movieNameSearch === undefined) {
-                movieNameSearch = 1;
-                callImdbSearch("Mr. Nobody");
-            }
-            else {
-                console.log("Title: " + response.data.Title + "\n" + "Year: " + response.data.Year + "\n" + "IMDB Rating: " + response.data.imdbRating + "\n" + "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n" + "Country: " + response.data.Country + "\n" + "Language: " + response.data.Language + "\n" + "Plot: " + response.data.Plot + "\n" + "Actors: " + response.data.Actors + "\n");
-                
-                appendFunc("Title: " + response.data.Title + "\n" + "Year: " + response.data.Year + "\n" + "IMDB Rating: " + response.data.imdbRating + "\n" + "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n" + "Country: " + response.data.Country + "\n" + "Language: " + response.data.Language + "\n" + "Plot: " + response.data.Plot + "\n" + "Actors: " + response.data.Actors + "\n");
-            }
+
+            console.log("Title: " + response.data.Title + "\n" + "Year: " + response.data.Year + "\n" + "IMDB Rating: " + response.data.imdbRating + "\n" + "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n" + "Country: " + response.data.Country + "\n" + "Language: " + response.data.Language + "\n" + "Plot: " + response.data.Plot + "\n" + "Actors: " + response.data.Actors + "\n");
+
+            appendFunc("Title: " + response.data.Title + "\n" + "Year: " + response.data.Year + "\n" + "IMDB Rating: " + response.data.imdbRating + "\n" + "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n" + "Country: " + response.data.Country + "\n" + "Language: " + response.data.Language + "\n" + "Plot: " + response.data.Plot + "\n" + "Actors: " + response.data.Actors + "\n");
+
 
         })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 function callConcertSearch(artistSearch) {
     var queryUrl = "https://rest.bandsintown.com/artists/" + artistSearch + "/events?app_id=codingbootcamp";
@@ -63,30 +52,44 @@ function callConcertSearch(artistSearch) {
 
                 appendFunc("Venue: " + response.data[i].venue.name + "\n" + "Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country + "\n" + "Date: " + moment(response.data[i].datetime, "YYYY-MM-DDTHH:mm:ss").format("L") + "\n");
             }
+            if (response.data.length === 0) {
+                console.log("No concerts were found for " + artistSearch);
+            }
         });
 }
-function appendFunc(textToAppend){
-    fs.appendFile("log.txt", textToAppend + "\n", function(error){
-        if(error){
+function appendFunc(textToAppend) {
+    fs.appendFile("log.txt", textToAppend + "\n", function (error) {
+        if (error) {
             console.log(error);
         }
-        
+
     })
 }
 
 
 if (firstArg === "concert-this") {
     var artist = process.argv.slice(3).join(" ");
-    console.log(artist);
+    if (!artist) {
+        console.log("You forgot to input an Artist.");
+        return;
+    }
     callConcertSearch(artist);
 
 } else if (firstArg === "spotify-this-song") {
 
     var spotQuery = process.argv.slice(3).join(" ");
+
+    //checks for no input and returns the sign ace of base to search if not found
+    if (!spotQuery) {
+        var spotQuery = "the sign ace of base"
+    }
     callSpotify(spotQuery);
 
 } else if (firstArg === "movie-this") {
     var movieName = process.argv.slice(3).join(" ");
+    if (!movieName) {
+        movieName = "Mr Nobody";
+    }
     callImdbSearch(movieName);
 
 } else if (firstArg === "do-what-it-says") {
@@ -103,7 +106,7 @@ if (firstArg === "concert-this") {
                 callSpotify(dataArray[i + 1]);
             } else if (dataArray[i] === "movie-this") {
                 callImdbSearch(dataArray[i + 1]);
-            } else if (dataArray[i] === "concert-this") {   
+            } else if (dataArray[i] === "concert-this") {
                 callConcertSearch(dataArray[i + 1]);
             }
         }
